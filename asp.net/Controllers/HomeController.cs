563ab3a18ff4eec1d5992f053085cae9272f2d37
Nazar-Kostrykin <nazar.kostrykin125@gmail.com>
2021-12-19 17:05:04 +0200

    added everything else

---
+++ asp.net/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace kostrykin
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var students = DataStudents.students;
+
+            return View(students);
+        }
+    }
+}
